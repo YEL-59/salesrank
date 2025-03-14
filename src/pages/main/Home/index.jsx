32b6cb7ea0c99530c90bc768c34b82dac448f8a9
tofayel <tofayeltuhin143@gmail.com>
2025-03-14 11:24:37 +0600

    add delevery section

--- src/pages/main/Home/index.jsx
+++ src/pages/main/Home/index.jsx
@@ -1,3 +1,4 @@
+import Delivery from "@/components/main/Home/delivery";
 import Hero from "@/components/main/Home/hero";
 import React from "react";
 
@@ -6,6 +7,7 @@ const Home = () => {
     <>
       <div>
         <Hero />
+        <Delivery />
       </div>
     </>
   );
