180b3b153f85ed6d16ffc256c88a417eac802950
Md. Tofayel Islam <tofayelislam19986@gmail.com>
2025-03-23 15:22:57 +0600

    add filter layout

--- src/pages/dashboard/candidate/index.jsx
+++ src/pages/dashboard/candidate/index.jsx
@@ -2,14 +2,40 @@ import Candidatetab from "@/components/dashboard/candidate/candidatetab";
 import { Candidatetable } from "@/components/dashboard/candidate/candidatetable";
 
 import Navbar from "@/components/dashboard/candidate/navbar";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const Candidate = () => {
   return (
     <>
-      <div className="p-5 bg-white rounded-lg h-screen">
-        <Navbar />
-        <Candidatetab />
-        <Candidatetable />
+      <div className="flex w-full gap-5">
+        <div className="p-5 bg-white rounded-lg h-screen flex-1">
+          <h1>Filters</h1>
+          <div className="border-b py-4">
+            {" "}
+            <Select>
+              <SelectTrigger className="w-full border-0">
+                <SelectValue placeholder="Department" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="light">Light</SelectItem>
+                <SelectItem value="dark">Dark</SelectItem>
+                <SelectItem value="system">System</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+        <div className="p-5 bg-white rounded-lg h-screen flex-1">
+          <Navbar />
+          <Candidatetab />
+
+          <Candidatetable />
+        </div>
       </div>
     </>
   );
